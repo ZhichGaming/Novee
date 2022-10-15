0aad830dd5d7d908a4fb3078517aefafbbe42e3d
ZhichGaming <nick.zhicheng@gmail.com>
2022-10-15 15:12:39 -0400

    Initial Commit

---
+++ Novee/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Novee
+//
+//  Created by Nick on 2022-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Novee/NoveeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoveeApp.swift
+//  Novee
+//
+//  Created by Nick on 2022-10-15.
+//
+
+import SwiftUI
+
+@main
+struct NoveeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
