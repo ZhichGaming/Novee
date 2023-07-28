8537cab650febd7f3a486ba770090e0bcb2df74c
ZhichGaming <nick.zhicheng@gmail.com>
2023-07-28 16:33:14 +0800

    Fix: Fix asurascans
    This took an embarassingly long time to fix.

--- Novee/Sources/Manga/AsuraScans.swift
+++ Novee/Sources/Manga/AsuraScans.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftSoup
 
 class AsuraScans: MangaFetcher, MangaSource {
-    override init(label: String = "AsuraScans", sourceId: String = "asurascans", baseUrl: String = "https://www.asurascans.com") {
+    override init(label: String = "AsuraScans", sourceId: String = "asurascans", baseUrl: String = "https://asura.gg") {
         super.init(label: label, sourceId: sourceId, baseUrl: baseUrl)
     }
     
