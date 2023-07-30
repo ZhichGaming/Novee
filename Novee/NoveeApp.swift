0f0c46f2457b8f4dc8127b32afeb93a8a9f0e26b
ZhichGaming <nick.zhicheng@gmail.com>
2023-07-31 00:29:13 +0800

    Feat: Fetching favourites every hour and notifs

--- Novee/NoveeApp.swift
+++ Novee/NoveeApp.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 import SystemNotification
+import UserNotifications
 
 @main
 struct NoveeApp: App {
     @StateObject var notification = SystemNotificationContext()
+    
+    init() {
+        loadSegmentFetcher()
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -85,5 +90,75 @@ struct NoveeApp: App {
     
     private func toggleFullScreen() {
         NSApp.keyWindow?.toggleFullScreen(nil)
+    }
+    
+    private func loadSegmentFetcher() {
+        requestNotificationAuthorization()
+        
+        let timer = Timer(fire: Date(), interval: 3600, repeats: true) { timer in
+            Task {
+                await fetchNewSegments()
+            }
+        }
+
+        RunLoop.current.add(timer, forMode: .default)
+    }
+    
+    private func requestNotificationAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                print("Notification authorization granted.")
+            } else if let error = error {
+                print("Notification authorization denied: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func fetchNewSegments() async {
+        let favourites = FavouritesVM.shared.getFavourites()
+        
+        var amountOfUpdates = 0
+        var updatedMediaTitle = ""
+        var updatedSegmentTitle = ""
+        
+        for favourite in favourites {
+            let savedLatestChapter = favourite.lastSegment
+            let fetchedLatestChapter = await FavouritesVM.shared.getLastFetchedSegment(for: favourite)
+            
+            if let savedLatestChapter = savedLatestChapter, let fetchedLatestChapter = fetchedLatestChapter {
+                if savedLatestChapter != fetchedLatestChapter {
+                    if amountOfUpdates == 0 {
+                        updatedMediaTitle = favourite.content.first?.value.title ?? "Unknown"
+                        updatedSegmentTitle = fetchedLatestChapter
+                    }
+
+                    amountOfUpdates += 1
+                }
+            }
+            
+            try? await Task.sleep(nanoseconds: UInt64(3 * Double(NSEC_PER_SEC)))
+        }
+        
+        if amountOfUpdates == 0 {
+            return
+        }
+        
+        do {
+            let content = UNMutableNotificationContent()
+            content.title = "There are " + amountOfUpdates.description + " new chapters/episodes"
+            content.subtitle = "\(updatedSegmentTitle) from \(updatedMediaTitle) and \(amountOfUpdates != 1 ? amountOfUpdates.description + " others " : "")have been published."
+            content.sound = UNNotificationSound.default
+
+            // show this notification five seconds from now
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+
+            // choose a random identifier
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+            // add our notification request
+            try await UNUserNotificationCenter.current().add(request)
+        } catch {
+            Log.shared.error(error)
+        }
     }
 }
